ce8e5ac9de63cc156983735bb513a4083e4df260
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-08-15 00:09:21 +0300

    finished lab13

--- songr/src/main/java/com/example/songr/HelloController.java
+++ songr/src/main/java/com/example/songr/HelloController.java
@@ -26,4 +26,5 @@ public class HelloController {
         return "Home.html";
     }
 
+
 }
--- songr/src/main/java/com/example/songr/SongRepository.java
+++ songr/src/main/java/com/example/songr/SongRepository.java
@@ -3,4 +3,6 @@ package com.example.songr;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SongRepository extends CrudRepository <Song,Integer>{
+
+    Iterable<Song> findAllByAlbumId(Integer id);
 }
